0893ce28a1fa5b5e7e8e279d600ce3533c92172b
Mess <felagunt@live.ru>
2025-02-24 12:25:34 +0300

    later

--- app/src/main/java/com/example/tvapp/presentation/TvShowDetail/components/EpisodeListItem.kt
+++ app/src/main/java/com/example/tvapp/presentation/TvShowDetail/components/EpisodeListItem.kt
@@ -1,13 +1,98 @@
 package com.example.tvapp.presentation.TvShowDetail.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 import com.example.tvapp.domain.models.Episode
 
 @Composable
 fun EpisodeListItem(
     episode: Episode,
+    onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-
+    Surface(
+        shape = RoundedCornerShape(32.dp),
+        modifier = modifier
+            .clickable(
+                onClick = onClick
+            ),
+        color = MaterialTheme.colorScheme.surfaceTint
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                modifier = Modifier
+                    .height(100.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                val painter = rememberAsyncImagePainter(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(episode.image)
+                        .size(coil.size.Size.ORIGINAL)
+                        .build()
+                )
+                if (painter.state is AsyncImagePainter.State.Loading) {
+                    CircularProgressIndicator()
+                } else {
+                    Image(
+                        painter = painter,
+                        contentDescription = "Tv show's ${episode.name} image",
+                        modifier = Modifier
+                            .alpha(1f)
+                            .clip(RoundedCornerShape(8.dp))
+                    )
+                }
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Name: ${episode.name}",
+                    style = MaterialTheme.typography.titleMedium,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "Avg rating: ${episode.rating}",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+            Text(
+                text = "e ${episode.number} of s ${episode.season}",
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
 }
